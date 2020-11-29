2fccf0a67726f314c5ea0d7f0a8f0f43e62f1a0a
wineT <tolya.bogomolov2019@gmail.com>
2020-11-29 14:58:20 +1000

    added chart

--- index.php
+++ index.php
@@ -12,26 +12,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <?php if(isset($_SESSION['logged_user'])): ?>
+
         <title>Панель управления</title>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-        <script src="js/chart.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
+
     </head>
     <body>
-        <? echo 'Добро пожаловать, '.$user -> login.'! '; ?> <a href="/php/logout.php">Выйти</a> <br>
 
-        <canvas id="chart" width="600" height="400"></canvas><br>
+        <?php echo 'Добро пожаловать, '.$user -> login.'! '; ?> <a href="/php/logout.php">Выйти</a> <br>
+
+        <div class="container">
+            <canvas id="myChart" width="600px" height="400px"></canvas>
+        </div>
+
+        <script src="js/chart.js"></script>
+
+        <br>
         <button onclick="getData()">Обновить</button>
         <p id="ajax_data"></p><br>
     
     </body>
 </html>
 
-    <?php else:?>
-    <title>Добро пожаловать!</title>
+    <?php else:?> <!-- Если пользователь не зарегестрирован, то показываем форму входа-->
+        <title>Добро пожаловать!</title>
     </head>
     <body>
-    <a href="php/login.php">Вход</a><br>
-    <a href="php/reg.php">Регистрация</a>
-</body>
+        <a href="php/login.php">Вход</a><br>
+        <a href="php/reg.php">Регистрация</a>
+    </body>
+
 </html>
 <?php endif;?>
--- js/chart.js
+++ js/chart.js
@@ -1,10 +1,24 @@
 const ADDRES = 'http://localhost/php/get_data.php'
 
+// Vertical bar chart
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+
+    data: {
+     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+     datasets: [{
+         label: '# of Votes',
+         data: [12, 19, 3, 5, 2, 3]
+    }]
+}
+});
+
 setInterval(getData, 60000);
 
 async function getData() {
-     let response = await fetch(ADDRES);
-     let content = await response.json();
+      let response = await fetch(ADDRES);
+      let content = await response.json();
 
-     return content;
-}
+      return content;
+ }
