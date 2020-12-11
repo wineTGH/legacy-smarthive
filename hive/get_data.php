b09bb1f5585bf3e44e30285f06143cce8ce73745
Tolya (wineT) <tolya.bogomolov2019@gmail.com>
2020-12-11 22:44:49 +1000

    добавлена проверка пользователя

--- hive/get_data.php
+++ hive/get_data.php
@@ -1,23 +1,18 @@
 <?php
 
     define('ROOT', str_replace('\\', '/', $_SERVER['DOCUMENT_ROOT']) .'/');
-
     include ROOT.'/libs/db.php';
 
     $user = $_SESSION['logged_user'];
 
     $hive = R::findOne($user -> id, ' hiveid = ? ORDER BY id DESC', [$user -> hiveid]);
 
-    // if ($user -> hiveid == $hive -> hiveid) {
-
-
-    // }
-
-    $arr = array(
-        'data' => $hive -> data
-    ); 
-
-    echo json_encode($arr);
-    exit;
-
+    if ($hive) {
+        $arr = array(
+            'data' => $hive -> data
+        ); 
+    
+        echo json_encode($arr);
+        exit;
+    }
 ?>
